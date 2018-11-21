e3334e8af8131fb147b167710d1f42a577e47eb9
defaultz12 <arielpasilang@gmail.com>
2018-11-22 00:11:48 +0800

    fixed a background

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,7 @@ export default () => (
                 <div class="row">
                     <div class="col-md-8">
                         <h3>Your Logo</h3>
-                        <p>© 2016 Your Company. Designed and Developed by</p>
+                        <p>© 2016 Your Company. Designed and Developed by: Ariel P:</p>
                     </div>
                     <div class="col-md-4">
                         <p class="text-right social"><i class="typcn typcn-social-facebook-circular"></i><i class="typcn typcn-social-twitter-circular"></i><i class="typcn typcn-social-tumbler-circular"></i><i class="typcn typcn-social-github-circular"></i><i class="typcn typcn-social-dribbble-circular"></i></p>
